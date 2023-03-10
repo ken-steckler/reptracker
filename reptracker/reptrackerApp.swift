bbaf276d7b02395eff932b4b28ade1afc545082f
Ken Steckler <steckler92ken@gmail.com>
2023-03-10 14:00:47 -0800

    Initial Commit

---
+++ reptracker/reptrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  reptrackerApp.swift
+//  reptracker
+//
+//  Created by Ken Steckler on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct reptrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
