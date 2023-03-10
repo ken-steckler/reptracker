bc6792904b2993d4ce31aad61cea316d051554b5
Ken Steckler <steckler92ken@gmail.com>
2023-03-10 15:29:01 -0800

    reptracker for iphone

--- reptracker/ContentView.swift
+++ reptracker/ContentView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count: Int = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button(action: {self.count += 1})
+                {
+                    Text("Increment").foregroundColor(Color.white)
+                        .frame(width: 300, height: 120)
+                        .padding()
+                        .background(Color.red)
+                        .cornerRadius(8)
+                        .shadow(color: .red, radius: 3, x: 1, y: 1)
+                }
+            Text("\(count)").font(.system(size: 120))
+            
+            Button(action: {
+                self.count = 0
+            }) {
+                Text("Reset").foregroundColor(Color.white)
+                    .frame(width: 300)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .shadow(color: .blue, radius: 3, x: 1, y: 1)
+            }
         }
     }
 }
